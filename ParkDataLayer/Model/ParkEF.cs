6994a82b257d92c94637cfc79a5e4ee59687b288
LiamT8 <91737599+TackLiam@users.noreply.github.com>
2023-12-14 08:44:44 +0100

    Start-14/12/2023

---
+++ ParkDataLayer/Model/ParkEF.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkDataLayer.Model
+{
+    public class ParkEF
+    {
+        [Key]
+        [MaxLength(255)]
+        public string id { get; set; }
+        public string name { get; set; }
+        public string location { get; set; }
+    }
+}
